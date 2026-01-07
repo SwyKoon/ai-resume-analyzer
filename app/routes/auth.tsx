f95c4f4de88015c0117a5441da5e9df3208b5f73
SwyKoon <snowswykoon@gmail.com>
2026-01-07 17:43:22 +0530

    Authentication

---
+++ app/routes/auth.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+
+import { usePuterStore } from "../lib/puter";
+
+export const meta = () => ([
+    { title: 'Resumate | Auth' },
+    { name: 'description', content: 'Log into you account' },
+])
+
+const auth = () => {
+    // State to check loading state from puter store
+    // It is getting reset whenever we use some other function, so good to have it here
+    const { isLoading, auth } = usePuterStore();
+
+    // Page redirection for 2 cases:
+    // a.) Users not logged in, to sign in
+    // b.) Users now logged in, to page of their choice (home if no choice)
+    const location = useLocation();
+    // Page they want to visit. Extract the first next page they want to visit
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    // Handle redirection in case the user is already signed in.
+    // If not signed in, we stay here
+    useEffect(() => {
+        if(auth.isAuthenticated) {
+            navigate(next);
+        }
+    }, [auth.isAuthenticated, next]);
+
+    
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center
+        justify-center">
+            {/* This is going to act as our form */}
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+
+                    {/* Auth Checking */}
+                    <div>
+                        { isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in ...</p>
+                            </button>
+                        ) : (
+                                // Inside empty React gragment to render JSX
+                                <>
+                                    { auth.isAuthenticated ? (
+                                        <button className="auth-button" 
+                                        onClick={ auth.signOut }>
+                                            <p>Log Out</p>
+                                        </button>
+                                    ) : (
+                                        <button className="auth-button" 
+                                        onClick={ auth.signIn }>
+                                            <p>Log In</p>
+                                        </button>
+                                    )}
+                                </>
+                            )
+                        }
+                    </div>
+                </section>
+            </div>
+
+        </main>
+    )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,59 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+
+import Navbar from "../components/Navbar";
+import ResumeCard from "../components/ResumeCard";
+import { resumes } from "../../constants";
+
+import { usePuterStore } from "../lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumate" },
+    { name: "description", content: "Smart resume builder for smart people" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  // State to check loading state from puter store
+  // It is getting reset whenever we use some other function, so good to have it here
+  const { auth } = usePuterStore();
+
+  // Asking users to first lofin to view the home page.
+  // We will redirect the user to login page
+  const navigate = useNavigate();
+
+  // Handle redirection to home page once signed in
+  // If not signed in, we stay here
+  useEffect(() => {
+      if(!auth.isAuthenticated) {
+          navigate('/auth?next=/');
+      }
+  }, [auth.isAuthenticated]);
+
+  return (
+    // Entire App
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      {/* Hero section */}
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications and Resume Ratings</h1>
+          <h2>Review your submissions and check AI powered feedback</h2>
+        </div>
+
+        {/* Resume we uploaded previously */}
+        { resumes.length > 0 && (
+          <div className="resumes-section">
+            { resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
